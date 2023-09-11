75c795c4e467476b954483608f95b9daa8a458ea
jeetkanodia <jeetkanodia@gmail.com>
2023-09-12 03:51:47 +0530

    put session data client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import User from "./components/User";
 
 const HomePage = () => {
   return (
     <>
-    <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <User />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
